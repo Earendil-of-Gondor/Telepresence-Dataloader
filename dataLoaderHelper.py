e794569d46d4dbc0cfd8c58e724c3e911fa67146
Ziheng Wang <69565972+Earendil-of-Gondor@users.noreply.github.com>
2023-05-08 14:58:48 -0400

    Add files via upload

--- dataLoaderHelper.py
+++ dataLoaderHelper.py
@@ -1,27 +1,37 @@
 import torch.nn.functional as F
 import os
+import shutil
 import cv2
 import numpy as np
 import torch
+import subprocess
+from llff.poses.pose_utils import run_colmap
 
-CAMERA_IDS = ['000228212212', '000655594512', '000665414412', '000738314412']
+CAMERA_IDS = ['000228212212', '000597713512', '000655594512', '000665414412', '000738314412', '000907513512']
+TEST_CAMERA_IDX = ['000597713512', '000907513512']
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# MARK: file
+
+# MARK: file source
 def get_trial_folder(args):
     return os.path.join(args.basedir, 'trial_{}'.format(args.trial_idx))
+
 
 def get_camera_folder(args, cam_id):
     return os.path.join(get_trial_folder(args), '{}'.format(cam_id))
 
+
 def getColorFolderPath(args, camId):
     return os.path.join(get_camera_folder(args, camId), 'color')
+
 
 def get_color_img(args, cam_id, img_i):
     return os.path.join(getColorFolderPath(args, cam_id), '{:05d}.png'.format(img_i))
 
+
 def getDepthFolderPath(args, camId):
     return os.path.join(get_camera_folder(args, camId), 'depth')
+
 
 def get_depth_img(args, cam_id, img_i):
     return os.path.join(getDepthFolderPath(args, cam_id), '{:05d}.pt'.format(img_i))
@@ -30,71 +40,131 @@ def get_depth_img(args, cam_id, img_i):
 def getBackgroundDepthFilePath(args, camId):
     return os.path.join(args.basedir, 'background', camId, 'depth', '00000.pt')
 
+
 def get_img_shape(args):
     img = cv2.imread(get_color_img(args, CAMERA_IDS[0], args.frame_start))
     return img.shape
+
 
 def get_depth_shape(args):
     img = torch.load(getBackgroundDepthFilePath(args, CAMERA_IDS[0]))['depth']
     return img.shape
 
-def getColorOutputFolderPath(args, output_type, **kwargs):
+
+# file des
+def concateStringRemoveBeginningDup(str1, str2):
+    if str1[-1] != '_': str1 = str1 + '_'
+    if str2.find(str1) > 0:
+        return str2
+    else:
+        return str1 + str2
+
+
+def get_base_output_path(args):
+    output_type = args.output_type
     if output_type == 0:
-        path = os.path.join(get_trial_folder(args), 'torf_data', 'color')
+        path = os.path.join(get_trial_folder(args), concateStringRemoveBeginningDup('torf_data', args.outdir))
     elif output_type == 1:
-        path = os.path.join(get_trial_folder(args), 'nerf_data', 'color')
+        path = os.path.join(get_trial_folder(args), concateStringRemoveBeginningDup('nerf_data', args.outdir))
     elif output_type == 2:
-        path = os.path.join(get_trial_folder(args), 'dnerf_data', 'frame_{:03d}'.format(kwargs['frame']), 'images')
+        path = os.path.join(get_trial_folder(args), concateStringRemoveBeginningDup('dnerf_data', args.outdir))
+    elif output_type == 3:
+        path = os.path.join(get_trial_folder(args), concateStringRemoveBeginningDup('tdnerf_data', args.outdir))
+    elif output_type == 4:
+        path = os.path.join(get_trial_folder(args), concateStringRemoveBeginningDup('mnsff_data', args.outdir))
     if not os.path.exists(path):
         os.makedirs(path)
     return path
 
 
-def getDistanceOutputFolderPath(args, output_type, **kwargs):
+def getCamOutputFolderPath(args, **kwargs):
+    output_type = args.output_type
     if output_type == 0:
-        path = os.path.join(get_trial_folder(args), 'torf_data', 'distance')
+        path = os.path.join(get_base_output_path(args), 'cams')
     elif output_type == 1:
-        path = os.path.join(get_trial_folder(args), 'nerf_data', 'color')
+        path = os.path.join(get_base_output_path(args), 'color')
     elif output_type == 2:
-        path = os.path.join(get_trial_folder(args), 'dnerf_data', 'frame_{:03d}'.format(kwargs['frame']), 'depths')
+        path = os.path.join(get_base_output_path(args), 'frame_{:03d}'.format(kwargs['frame']))
+    elif output_type == 3:
+        path = os.path.join(get_base_output_path(args))
+    elif output_type == 4:
+        path = os.path.join(get_base_output_path(args))
     if not os.path.exists(path):
         os.makedirs(path)
     return path
 
 
-def getColorMaskOutputFolderPath(args, output_type, **kwargs):
+def getColorOutputFolderPath(args, **kwargs):
+    output_type = args.output_type
     if output_type == 0:
-        path = os.path.join(get_trial_folder(args), 'torf_data', 'color_mask')
+        path = os.path.join(get_base_output_path(args), 'color')
     elif output_type == 1:
-        path = os.path.join(get_trial_folder(args), 'nerf_data', 'color')
+        path = os.path.join(get_base_output_path(args), 'color')
     elif output_type == 2:
-        path = os.path.join(get_trial_folder(args), 'dnerf_data', 'frame_{:03d}'.format(kwargs['frame']), 'color_mask')
+        path = os.path.join(get_base_output_path(args),
+                            'frame_{:03d}'.format(kwargs['frame']), 'images')
+    elif output_type == 3:
+        path = os.path.join(get_base_output_path(args), 'images')
+    elif output_type == 4:
+        path = os.path.join(get_base_output_path(args), 'images')
     if not os.path.exists(path):
         os.makedirs(path)
     return path
 
 
-def getDistanceMaskOutputFolderPath(args, output_type, **kwargs):
+def getDistanceOutputFolderPath(args, **kwargs):
+    output_type = args.output_type
     if output_type == 0:
-        path = os.path.join(get_trial_folder(args), 'torf_data', 'distance_mask')
+        path = os.path.join(get_base_output_path(args), 'distance')
     elif output_type == 1:
-        path = os.path.join(get_trial_folder(args), 'nerf_data', 'color')
+        path = os.path.join(get_base_output_path(args), 'color')
     elif output_type == 2:
-        path = os.path.join(get_trial_folder(args), 'dnerf_data', 'frame_{:03d}'.format(kwargs['frame']), 'distance_mask')
+        path = os.path.join(get_base_output_path(args),
+                            'frame_{:03d}'.format(kwargs['frame']), 'depths')
+    elif output_type == 3:
+        path = os.path.join(get_base_output_path(args), 'depths')
+    elif output_type == 4:
+        path = os.path.join(get_base_output_path(args), 'disp')
     if not os.path.exists(path):
         os.makedirs(path)
     return path
 
-def getCamOutputFolderPath(args, output_type, **kwargs):
+
+def getColorMaskOutputFolderPath(args, **kwargs):
+    output_type = args.output_type
     if output_type == 0:
-        path = os.path.join(get_trial_folder(args), 'torf_data', 'cams')
+        path = os.path.join(get_base_output_path(args), 'color_mask')
     elif output_type == 1:
-        path = os.path.join(get_trial_folder(args), 'nerf_data', 'color')
+        path = os.path.join(get_base_output_path(args), 'color')
     elif output_type == 2:
-        path = os.path.join(get_trial_folder(args), 'dnerf_data', 'frame_{:03d}'.format(kwargs['frame']))
+        path = os.path.join(get_base_output_path(args),
+                            'frame_{:03d}'.format(kwargs['frame']), 'color_mask')
+    elif output_type == 3:
+        path = os.path.join(get_base_output_path(args), 'color_mask')
+    elif output_type == 4:
+        path = os.path.join(get_base_output_path(args), 'color_mask')
     if not os.path.exists(path):
         os.makedirs(path)
     return path
+
+
+def getDistanceMaskOutputFolderPath(args, **kwargs):
+    output_type = args.output_type
+    if output_type == 0:
+        path = os.path.join(get_base_output_path(args), 'distance_mask')
+    elif output_type == 1:
+        path = os.path.join(get_base_output_path(args), 'color')
+    elif output_type == 2:
+        path = os.path.join(get_base_output_path(args),
+                            'frame_{:03d}'.format(kwargs['frame']), 'distance_mask')
+    elif output_type == 3:
+        path = os.path.join(get_base_output_path(args), 'distance_mask')
+    elif output_type == 4:
+        path = os.path.join(get_base_output_path(args), 'distance_mask')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
 
 # MARK: camera and pose
 def convert_to_w2c_extrinsic(tc2w, Rc2w):
@@ -105,12 +175,25 @@ def convert_to_w2c_extrinsic(tc2w, Rc2w):
     extrinsic[:3, [-1]] = tw2c
     return extrinsic
 
+
 def make_intrinsic(K, factor):
     color_intrinsics = np.eye(4)
     color_intrinsics[:3, :3] = K
     color_intrinsics /= factor
     color_intrinsics[[2, 3], [2, 3]] = 1
     return color_intrinsics
+
+
+def make_poses_bounds(color_intrinsics, tc2w, Rc2w, bds):
+    # poses = np.concatenate([poses[:, 1:2, :], -poses[:, 0:1, :], poses[:, 2:, :]], 1)
+    pose = np.zeros((3, 5))
+    pose[:3, :3] = Rc2w
+    pose[:3, [3]] = tc2w
+    pose[2, 4] = color_intrinsics[0, 0]
+    pose = np.concatenate([pose[:, 1:2], pose[:, 0:1], -pose[:, 2:3], pose[:, 3:]], 1)
+    pose = pose.flatten()
+    pose = np.concatenate([pose, bds], 0)
+    return pose
 
 
 # MARK: depth processing
@@ -130,6 +213,7 @@ def load_sparse_depth(d_pix_in, d_pix_indices_in, hw):
             n_added += idx_cur_row.shape[0]
     return cur_depth
 
+
 def compute_distance_map(depth_map, k, depth_mask):
     cx = k[0, 2]
     cy = k[1, 2]
@@ -144,15 +228,22 @@ def compute_distance_map(depth_map, k, depth_mask):
 
     return distance_map
 
+
 def depth_to_distance_single_point(u, v, d, cx, cy, f):
     factor = d / f
     return np.linalg.norm(np.array([factor * (u - cx), factor * (v - cy), d]))
 
-def compute_merged_depth_scaled(args, cam_id, img_i, orig_sh, target_sh, bg):
+
+def compute_depth(args, cam_id, img_i, orig_sh):
     data = torch.load(get_depth_img(args, cam_id, img_i))
     cur_d_pixs = data['foreground_depth'].float()
     dmap_idx = data['foreground_depth_indices']
     dmap = load_sparse_depth(cur_d_pixs, dmap_idx, orig_sh[:2])
+    return dmap
+
+
+def compute_merged_depth_scaled(args, cam_id, img_i, orig_sh, target_sh, bg):
+    dmap = compute_depth(args, cam_id, img_i, orig_sh)
 
     bg_mask = (dmap > 0)  # true means we have dynamic depth, don't use bg depth
     bg[bg_mask] = 0
@@ -162,14 +253,17 @@ def compute_merged_depth_scaled(args, cam_id, img_i, orig_sh, target_sh, bg):
 
     return scaled_depth
 
+
 def make_np_distance_and_mask(args, cam_id, img_i, k, orig_sh, target_sh, bg):
+    if cam_id in TEST_CAMERA_IDX:
+        return np.zeros(target_sh[:2]), np.zeros(target_sh[:2]).astype(bool), np.zeros(target_sh[:2])
+
     scaled_depth = compute_merged_depth_scaled(args, cam_id, img_i, orig_sh, target_sh, bg)
 
-    depth_mask = (scaled_depth != 0)  # true means there is depth
+    depth_mask = (scaled_depth > 1e-5)  # true means there is depth
 
     np_scaled_depth = scaled_depth.cpu().detach().numpy().astype(np.float32)
     np_depth_mask = depth_mask.cpu().detach().numpy()
     np_distance = compute_distance_map(np_scaled_depth, k, np_depth_mask)
 
     return np_scaled_depth, np_depth_mask, np_distance
-
--- unifiedDataLoader.py
+++ unifiedDataLoader.py
@@ -1,4 +1,32 @@
+import os
+
 from dataLoaderHelper import *
+
+'''
+setup_xxxx_xxx:
+    trial_1:
+        camid:
+            depth:
+                00000.pt
+                00001.pt
+                ...
+            color
+                00000.png
+                00001.png
+                ...
+    trial_2:
+    ...
+    background:
+        camid:
+            depth:
+                00000.pt
+            color
+                00000.png
+        camid:
+        ...
+    calib.npy
+'''
+
 
 def config_parser():
     import configargparse
@@ -11,8 +39,13 @@ def config_parser():
                         help='trial index')
     parser.add_argument("--structure_type", type=int,
                         help='file structure type. default 0 for separate background and foreground depth')
-    parser.add_argument("--output_type", type=int,
-                        help='output file structure type. 0 for torf. 1 for nerf. 2 for dnerf')
+
+    parser.add_argument("--outdir", type=str,
+                        help='output directory name inside trial folder')
+    parser.add_argument("--output_type", type=str,
+                        help='seperate by , output file structure type. 0 for torf. 1 for nerf. 2 for dnerf. 3 for tdnerf, '
+                             '4 for mnsff')
+
     parser.add_argument("--frame_start", type=int,
                         help='start index including')
     parser.add_argument("--frame_end", type=int,
@@ -36,16 +69,16 @@ def torf_save_depth_and_distance(args, i, cam_id, img_i, k, orig_sh, target_sh, 
     np_scaled_depth, np_depth_mask, np_distance = make_np_distance_and_mask(args, cam_id, img_i, k, orig_sh, target_sh,
                                                                             bg)
 
-    np.save(torf_output_file_path_formatter(getDistanceOutputFolderPath(args, 0), i, cam_id, frame_length), np_distance)
-    np.save(torf_output_file_path_formatter(getDistanceMaskOutputFolderPath(args, 0), i, cam_id, frame_length), np_depth_mask)
+    np.save(torf_output_file_path_formatter(getDistanceOutputFolderPath(args), i, cam_id, frame_length), np_distance)
+    np.save(torf_output_file_path_formatter(getDistanceMaskOutputFolderPath(args), i, cam_id, frame_length), np_depth_mask)
 
 
 def torf_save_color(args, i, cam_id, img_i, target_sh, frame_length):
     color = cv2.imread(get_color_img(args, cam_id, img_i))
     color = cv2.resize(color, target_sh[1::-1]) # opencv input dim is (width, height)
     color_mask = (color != 0).any(2)
-    np.save(torf_output_file_path_formatter(getColorOutputFolderPath(args, 0), i, cam_id, frame_length), color)
-    np.save(torf_output_file_path_formatter(getColorMaskOutputFolderPath(args, 0), i, cam_id, frame_length), color_mask)
+    np.save(torf_output_file_path_formatter(getColorOutputFolderPath(args), i, cam_id, frame_length), color)
+    np.save(torf_output_file_path_formatter(getColorMaskOutputFolderPath(args), i, cam_id, frame_length), color_mask)
     return color
 
 # MARK: dnerf specific
@@ -55,8 +88,8 @@ def dnerf_save_depth_and_distance(args, i, cam_id, img_i, k, orig_sh, target_sh,
     kwargs = {
         'frame': i
     }
-    np.save(os.path.join(getDistanceOutputFolderPath(args, 2, **kwargs), '{}.npy'.format(cam_id)), np_distance)
-    np.save(os.path.join(getDistanceMaskOutputFolderPath(args, 2, **kwargs), '{}.npy'.format(cam_id)), np_depth_mask)
+    np.save(os.path.join(getDistanceOutputFolderPath(args, **kwargs), '{}.npy'.format(cam_id)), np_distance)
+    np.save(os.path.join(getDistanceMaskOutputFolderPath(args, **kwargs), '{}.npy'.format(cam_id)), np_depth_mask)
 
 
 def dnerf_save_color(args, i, cam_id, img_i, target_sh, frame_length):
@@ -67,8 +100,47 @@ def dnerf_save_color(args, i, cam_id, img_i, target_sh, frame_length):
     kwargs = {
         'frame': i
     }
-    cv2.imwrite(os.path.join(getColorOutputFolderPath(args, 2, **kwargs), '{}.png'.format(cam_id)), color)
-    np.save(os.path.join(getColorMaskOutputFolderPath(args, 2, **kwargs), '{}.npy'.format(cam_id)), color_mask)
+    cv2.imwrite(os.path.join(getColorOutputFolderPath(args, **kwargs), '{}.png'.format(cam_id)), color)
+    np.save(os.path.join(getColorMaskOutputFolderPath(args, **kwargs), '{}.npy'.format(cam_id)), color_mask)
+    return color
+
+# MARK: tdnerf
+def multiview_time_nerf_output_file_path_formatter(basePath, i, cam_id, frame_length):
+    return os.path.join(basePath, '{:05d}.npy'.format(i + frame_length * CAMERA_IDS.index(cam_id)))
+
+
+def multiview_time_nerf_save_depth_and_distance(args, i, cam_id, img_i, k, orig_sh, target_sh, bg, frame_length):
+    np_scaled_depth, np_depth_mask, np_distance = make_np_distance_and_mask(args, cam_id, img_i, k, orig_sh, target_sh,
+                                                                            bg)
+    bds_min = np.amin(np_distance)
+    bds_max = np.amax(np_distance)
+    bds = np.stack([bds_min, bds_max], 0)
+    np.save(multiview_time_nerf_output_file_path_formatter(getDistanceOutputFolderPath(args), i, cam_id, frame_length), np_distance)
+    np.save(multiview_time_nerf_output_file_path_formatter(getDistanceMaskOutputFolderPath(args), i, cam_id, frame_length), np_depth_mask)
+    return bds
+
+
+def tdnerf_save_color(args, i, cam_id, img_i, target_sh, frame_length):
+    color = cv2.imread(get_color_img(args, cam_id, img_i))
+    color = cv2.resize(color, target_sh[1::-1]) # opencv input dim is (width, height)
+    color_mask = (color > 0.1).any(2)
+    np.save(multiview_time_nerf_output_file_path_formatter(getColorOutputFolderPath(args), i, cam_id, frame_length), color)
+    np.save(multiview_time_nerf_output_file_path_formatter(getColorMaskOutputFolderPath(args), i, cam_id, frame_length), color_mask)
+    return color
+
+# MARK: mnsff
+def mnsff_save_color(args, i, cam_id, img_i, target_sh, frame_length):
+    color = cv2.imread(get_color_img(args, cam_id, img_i))
+    cv2.imwrite(
+        os.path.join(getColorOutputFolderPath(args), '{:05d}.png'.format(i + frame_length * CAMERA_IDS.index(cam_id))),
+        color)
+    color = cv2.resize(color, target_sh[1::-1]) # opencv input dim is (width, height)
+    color_mask = (color != 0).any(2)
+    resized_path = os.path.join(get_base_output_path(args), f'images_{target_sh[1]}x{target_sh[0]}')
+    if not os.path.exists(resized_path):
+        os.makedirs(resized_path)
+    cv2.imwrite(os.path.join(resized_path, '{:05d}.png'.format(i + frame_length * CAMERA_IDS.index(cam_id))), color)
+    np.save(multiview_time_nerf_output_file_path_formatter(getColorMaskOutputFolderPath(args), i, cam_id, frame_length), color_mask)
     return color
 
 # MARK: data loaders
@@ -99,9 +171,9 @@ def make_torf_data(args, factor, frames, frames_length, save_video, img_sh, dept
     extrinsics = []
 
     if save_video:
-        video = cv2.VideoWriter(os.path.join(args.basedir, 'trial_{}/preview/rgb.mp4'.format(args.trial_idx)),
+        video = cv2.VideoWriter(os.path.join(get_base_output_path(args), 'rgb.mp4'),
                                 cv2.VideoWriter_fourcc(*'mp4v'),
-                                15, (512, 384))
+                                15, target_sh[:2])
 
     for cam_id in CAMERA_IDS:
         # intrinsics
@@ -123,11 +195,13 @@ def make_torf_data(args, factor, frames, frames_length, save_video, img_sh, dept
     if save_video:
         video.release()
 
-    np.save(os.path.join(getCamOutputFolderPath(args, 0), 'color_extrinsics.npy'), extrinsics)
-    np.save(os.path.join(getCamOutputFolderPath(args, 0), 'tof_extrinsics.npy'), extrinsics)
+    np.save(os.path.join(getCamOutputFolderPath(args), 'color_extrinsics.npy'), extrinsics)
+    np.save(os.path.join(getCamOutputFolderPath(args), 'tof_extrinsics.npy'), extrinsics)
 
-    np.save(os.path.join(getCamOutputFolderPath(args, 0), 'color_intrinsics.npy'), color_intrinsics)
-    np.save(os.path.join(getCamOutputFolderPath(args, 0), 'tof_intrinsics.npy'), color_intrinsics)
+    np.save(os.path.join(getCamOutputFolderPath(args), 'color_intrinsics.npy'), color_intrinsics)
+    np.save(os.path.join(getCamOutputFolderPath(args), 'tof_intrinsics.npy'), color_intrinsics)
+
+    np.save(os.path.join(get_base_output_path(args), 'test_poses.npy'), np.load('test_poses.npy'))
 
 def make_nerf_data(args, factor, frames, frames_length, save_video, img_sh, depth_sh, target_sh):
     return 0
@@ -157,9 +231,9 @@ def make_dnerf_data(args, factor, frames, frames_length, save_video, img_sh, dep
     camera_data = np.load(os.path.join(args.basedir, 'calib.npy'), allow_pickle=True).item()
 
     if save_video:
-        video = cv2.VideoWriter(os.path.join(args.basedir, 'trial_{}/preview/rgb.mp4'.format(args.trial_idx)),
+        video = cv2.VideoWriter(os.path.join(get_base_output_path(args), 'rgb.mp4'),
                                 cv2.VideoWriter_fourcc(*'mp4v'),
-                                15, (512, 384))
+                                15, target_sh[:2])
 
     for cam_id in CAMERA_IDS:
         # intrinsics
@@ -178,13 +252,171 @@ def make_dnerf_data(args, factor, frames, frames_length, save_video, img_sh, dep
             kwargs = {
                 'frame': i
             }
-            np.save(os.path.join(getCamOutputFolderPath(args, 2, **kwargs), 'calib.npy'), camera_data)
+            np.save(os.path.join(getCamOutputFolderPath(args, **kwargs), 'calib.npy'), camera_data)
             if save_video:
                 video.write(color_frame)
 
     if save_video:
         video.release()
 
+def make_tdnerf_data(args, factor, frames, frames_length, save_video, img_sh, depth_sh, target_sh):
+    '''
+
+    :param args:
+    :param factor:
+    :param frames:
+    :param frames_length:
+    :param save_video:
+    :param img_sh:
+    :param depth_sh:
+    :param target_sh:
+    :return: -tdnerf_data (all npy files)
+                -poses_bounds.npy
+                -images: len(cams) * frames_length
+                -depths:
+                -color_mask
+                -distance_mask
+
+    '''
+    camera_data = np.load(os.path.join(args.basedir, 'calib.npy'), allow_pickle=True).item()
+
+    extrinsics = []
+
+    if save_video:
+        video = cv2.VideoWriter(os.path.join(get_base_output_path(args), 'rgb.mp4'),
+                                cv2.VideoWriter_fourcc(*'mp4v'),
+                                15, target_sh[:2])
+
+    poses_bounds = []
+    for cam_id in CAMERA_IDS:
+        # intrinsics
+        color_intrinsics = make_intrinsic(camera_data[cam_id]['K_RGB'], factor)
+
+        print(color_intrinsics)
+
+        depth_bg = getBackgroundDepthFilePath(args, cam_id)
+        depth_bg = torch.load(depth_bg)['depth'].to(device)
+
+        for i, img_i in enumerate(frames):
+            color_frame = tdnerf_save_color(args, i, cam_id, img_i, target_sh, frames_length)
+            bds = multiview_time_nerf_save_depth_and_distance(args, i, cam_id, img_i, color_intrinsics, depth_sh, target_sh, depth_bg, frames_length)
+            poses_bounds.append(make_poses_bounds(color_intrinsics, camera_data[cam_id]['T_to_ref_RGB'],
+                                                  camera_data[cam_id]['R_to_ref_RGB'], bds))
+            if save_video:
+                video.write(color_frame)
+
+    poses_bounds = np.stack(poses_bounds, 0)
+    # print(poses_bounds[::])
+    np.save(os.path.join(getCamOutputFolderPath(args), 'poses_bounds.npy'), poses_bounds)
+    if save_video:
+        video.release()
+
+def make_mnsff_data(args, factor, frames, frames_length, save_video, img_sh, depth_sh, target_sh):
+    '''
+
+    :param args:
+    :param factor:
+    :param frames:
+    :param frames_length:
+    :param save_video:
+    :param img_sh:
+    :param depth_sh:
+    :param target_sh:
+    :return: -tdnerf_data
+                -poses_bounds.npy
+                -images(png): len(cams) * frames_length
+                -images_123x123(png): len(cams) * frames_length
+                -disp(npy):
+                -color_mask
+                -distance_mask
+                -motion_masks(npy)
+                -flow_i1(npz)
+
+
+    '''
+    if True:
+        camera_data = np.load(os.path.join(args.basedir, 'calib.npy'), allow_pickle=True).item()
+
+        extrinsics = []
+
+        if save_video:
+            video = cv2.VideoWriter(os.path.join(get_base_output_path(args), 'rgb.mp4'),
+                                    cv2.VideoWriter_fourcc(*'mp4v'),
+                                    15, target_sh[:2])
+
+        poses_bounds = []
+        for cam_id in CAMERA_IDS:
+            # intrinsics
+            color_intrinsics = make_intrinsic(camera_data[cam_id]['K_RGB'], factor)
+
+            print(color_intrinsics)
+
+            depth_bg = getBackgroundDepthFilePath(args, cam_id)
+            depth_bg = torch.load(depth_bg)['depth'].to(device)
+
+            for i, img_i in enumerate(frames):
+                bds = multiview_time_nerf_save_depth_and_distance(args, i, cam_id, img_i, color_intrinsics, depth_sh, target_sh, depth_bg, frames_length)
+                color_frame = mnsff_save_color(args, i, cam_id, img_i, target_sh, frames_length)
+                poses_bounds.append(make_poses_bounds(color_intrinsics, camera_data[cam_id]['T_to_ref_RGB'],
+                                                      camera_data[cam_id]['R_to_ref_RGB'], bds))
+                if save_video:
+                    video.write(color_frame)
+
+        poses_bounds = np.stack(poses_bounds, 0)
+        # print(poses_bounds[::])
+        np.save(os.path.join(getCamOutputFolderPath(args), 'poses_bounds.npy'), poses_bounds)
+
+        if save_video:
+            video.release()
+
+    # colmap
+    # files_needed = ['{}.bin'.format(f) for f in ['cameras', 'images', 'points3D']]
+    #
+    # if os.path.exists(os.path.join(getCamOutputFolderPath(args), 'sparse/0')):
+    #     files_had = os.listdir(os.path.join(getCamOutputFolderPath(args), 'sparse/0'))
+    # else:
+    #     files_had = []
+    # if not all([f in files_had for f in files_needed]):
+    #     print('Need to run COLMAP')
+    #     run_colmap(getCamOutputFolderPath(args), 'exhaustive_matcher')
+    # else:
+    #     print('Don\'t need to run COLMAP')
+    #
+    # sparse_p0 = os.path.join(getCamOutputFolderPath(args), 'sparse', '0')
+    # if os.path.exists(sparse_p0):
+    #     print([(subprocess.check_output(['mv', sparse_p0+f'/{f}', os.path.join(getCamOutputFolderPath(args), 'sparse')],
+    #                                    universal_newlines=True)) for f in os.listdir(sparse_p0)])
+    #     os.rmdir(sparse_p0)
+
+    # image_undistorter_args = [
+    #     'colmap', 'image_undistorter',
+    #     '--image_path', os.path.join(getCamOutputFolderPath(args), 'images'),
+    #     '--input_path', os.path.join(getCamOutputFolderPath(args), 'sparse', '0'),
+    #     '--output_path', os.path.join(getCamOutputFolderPath(args), 'dense'),
+    #     '--output_type', 'COLMAP',
+    #     '--max_image_size', '2000'
+    # ]
+    #
+    # match_output = (subprocess.check_output(image_undistorter_args, universal_newlines=True))
+    # print('undistorted', match_output)
+
+
+
+    # motion mask and scene flow
+    wd = os.getcwd()
+    os.chdir(os.path.join(wd, 'nsff_scripts'))
+
+    if not os.path.exists('models/raft-things.pth'):
+        download_model_output = (subprocess.check_output(['./download_models.sh'], universal_newlines=True))
+        print(download_model_output)
+
+    motion_mask_args = ['python', 'run_flows_video.py',
+                        '--model', 'models/raft-things.pth',
+                        '--data_path', get_base_output_path(args)]
+
+    motion_mask_output = (subprocess.check_output(motion_mask_args, universal_newlines=True))
+    print(motion_mask_output)
+    os.chdir(wd)
 
 if __name__ == '__main__':
     parser = config_parser()
@@ -209,12 +441,17 @@ if __name__ == '__main__':
         'target_sh': target_sh
     }
 
-    if args.output_type == 0:
-        # torf data
-        make_torf_data(args, **kwargs)
+    outputs = [int(i) for i in args.output_type.split(',')]
 
-    elif args.output_type == 1:
-        make_nerf_data(args, **kwargs)
-
-    elif args.output_type == 2:
-        make_dnerf_data(args, **kwargs)
+    for output_type in outputs:
+        args.output_type = output_type
+        if output_type == 0:
+            make_torf_data(args, **kwargs)
+        elif output_type == 1:
+            make_nerf_data(args, **kwargs)
+        elif output_type == 2:
+            make_dnerf_data(args, **kwargs)
+        elif output_type == 3:
+            make_tdnerf_data(args, **kwargs)
+        elif output_type == 4:
+            make_mnsff_data(args, **kwargs)
